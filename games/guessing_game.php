6719086feabcfcf2bb2fec888353780662649d7d
BakedPotat <mishacccp98@gmail.com>
2021-10-22 21:48:46 -0600

    Primera parte TCU

---
+++ games/guessing_game.php
@@ -0,0 +1,63 @@
+<?php
+include_once("guessing_game_style.inc");
+$ArrayWords = array("bateria", "puntos de reciclaje");
+$ArrayClues = array("Es un componente muy contaminante, ya que suele contener litio", "puntos de reciclaje");
+$ArrayPics = array("img/bateria_litio.jpg", "img/p_re.jpg", "img/game_over.jpg");
+
+
+
+$index = isset($_POST['index']) ? $_POST['index'] : 0;
+$message2 = str_shuffle($ArrayWords[$index]);
+$message3 = ($ArrayClues[$index]);
+if(!isset($_POST['guess'])){
+    $message1="<h1>¡Bienvenido al juego de las palabras desordenadas!</h1></br>";
+    //$message3=str_shuffle($strArrayClues[$index]);
+    $message2=str_shuffle($ArrayWords[$index]);
+    echo '<img src="' . $ArrayPics[$index] . '" width="300" height="300" />';
+}
+
+
+elseif($_POST['guess'] == $ArrayWords[$index]){   
+    $message1="<h1>¡Bienvenido al juego de las palabras desordenadas!</h1>";
+    $index++;
+    if ($index < count($ArrayWords)) {
+        $message2=str_shuffle($ArrayWords[$index]);
+        $message3 = ($ArrayClues[$index]);
+    }
+    echo '<img src="' . $ArrayPics[$index] . '" width="300" height="300" />';
+}
+
+elseif($_POST['guess'] != $ArrayWords[$index]){
+    $message1="<h1>Estuvo cerca, intentá de nuevo</h1></br>";
+    echo '<img src="' . $ArrayPics[$index] . '" width="300" height="300" />';
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head><title>Palabras desordenadas</title></head>
+<meta charset="utf-8"><!-- Your HTML file can still use UTF-8-->
+<body>
+<?php 
+echo $message1; 
+if ($index < count($ArrayWords)) {
+    ?>
+    <center>
+    <b>Intentá encontrar cual es la palabra desordenada:</b>" <?php echo "<h3> $message2;</h3>"?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+    <b>Pista:</b>" <?php echo "<h2>$message3; </h2>"?>
+    <p><label for="guess">¿Cuál crees que es la palabra?:</label>
+    <input oninput="this.value = this.value.toLowerCase()" autofocus type="text" id="guess" name="guess" /></p>
+    <input type="hidden" name="index" value="<?php echo $index; ?>">
+    <button type="submit" name="submit" value="submit">Adivinar</button>
+    </center>
+    <?php
+} else {
+    ?>
+    <b>¡Fin del juego!</b>
+    <p><a href="<?php echo $_SERVER['PHP_SELF']; ?>">Jugar de nuevo</a></p>
+    <?php
+}
+?>
+</body></html>
---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+/* Template: Mark Portfolio Website Bootstrap HTML Template
+   Description: Custom JS file
+*/
+
+
+(function($) {
+    "use strict"; 
+	
+    
+
+
+    /* Navbar Scripts */
+    // jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function() {
+		if ($(".navbar").offset().top > 60) {
+			$(".fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".fixed-top").removeClass("top-nav-collapse");
+		}
+    });
+    
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+    });
+
+    // offcanvas script from Bootstrap + added element to close menu on click in small viewport
+    $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function () {
+        $('.offcanvas-collapse').toggleClass('open')
+    })
+
+    // hover in desktop mode
+    function toggleDropdown (e) {
+        const _d = $(e.target).closest('.dropdown'),
+            _m = $('.dropdown-menu', _d);
+        setTimeout(function(){
+            const shouldOpen = e.type !== 'click' && _d.is(':hover');
+            _m.toggleClass('show', shouldOpen);
+            _d.toggleClass('show', shouldOpen);
+            $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+        }, e.type === 'mouseleave' ? 300 : 0);
+    }
+    $('body')
+    .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+    .on('click', '.dropdown-menu a', toggleDropdown);
+
+
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function(){
+		if ($(this).val() != '') {
+			$(this).addClass('notEmpty');
+		} else {
+			$(this).removeClass('notEmpty');
+		}
+	});
+	
+
+    /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<div id="contenedor" class="contenedor"><input type="checkbox" id ="btn-mas"><div class="redes"> <a target="_blanck" href="mailto:contackya@gmail.com" class="icon-mail"></a><a  target="_blanck" href="https://api.whatsapp.com/send?phone=50686966849&amp;text=Hola!&nbsp;Me gustaría consultar&nbsp;sobre los servicios que ofrecen"  class="icon-whatsapp"></a></div><div class="btn-mas"><label for="btn-mas" class="icon-chat"></label></div></div>');
+    var amountScrolled = 700;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('div.contenedor').fadeIn('500');
+        } else {
+            $('div.contenedor').fadeOut('500');
+        }
+    });
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function() {
+		$(this).blur();
+	});
+
+})(jQuery);
