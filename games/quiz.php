894180944ff3413ada214d865e5e94e6b778a709
BakedPotat <mishacccp98@gmail.com>
2021-11-17 19:43:20 -0600

    Added quiz game

---
+++ games/quiz.php
@@ -0,0 +1,161 @@
+<!--code by webdevtrick (webdevtrick.com) -->
+<head>
+ <meta charset=UTF-8" />
+ 
+ <title>Quiz - Desechos electronicos</title>
+ <link rel="stylesheet" type="text/css" href="style-quiz.css" />
+ 
+</head>
+ 
+<body>
+
+ <div id="page-wrap">
+ 
+ <h1>La importancia del manejo de desechos electronicos</h1>
+ 
+ <form action="resultado.php" method="post" id="quiz">
+ 
+            <ol>
+            
+                <li>
+                
+                    <h3>¿En dónde suelen terminar los desechos electrónicos?</h3>
+                    
+                    <div>
+                        <input type="radio" checked=":" name="question-1-answers" id="question-1-answers-A" value="A"  />
+                        <label for="question-1-answers-A">A) Playas </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) Calles</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) Ríos</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D">D) Parques</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>¿Cuál es una de las consecuencias de los desechos electrónicos?</h3>
+                    
+                    <div>
+                        <input type="radio" checked=":" name="question-2-answers" id="question-2-answers-A" value="A" />
+                        <label for="question-2-answers-A">A) Mejor calidad de vida</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+                        <label for="question-2-answers-B">B) Contaminación en tierra, agua y aire</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+                        <label for="question-2-answers-C">C) Beneficios económicos</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
+                        <label for="question-2-answers-D">D) Venta de electrodomésticos</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>¿Cuál se considera como uno de los materiales más contaminantes provenientes de desechos electrónicos?</h3>
+                    
+                    <div>
+                        <input type="radio" checked=":" name="question-3-answers" id="question-3-answers-A" value="A" />
+                        <label for="question-3-answers-A">A) Cloro</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+                        <label for="question-3-answers-B">B) Fósforo</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+                        <label for="question-3-answers-C">C) Dióxido de carbono</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-D" value="D" />
+                        <label for="question-3-answers-D">D) Mercurio</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>¿Cuál enfermedad se suele asociar con los desechos electrónicos?</h3>
+                    
+                    <div>
+                        <input type="radio" checked=":" name="question-4-answers" id="question-4-answers-A" value="A" />
+                        <label for="question-4-answers-A">A) Cáncer</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
+                        <label for="question-4-answers-B">B) Asma</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
+                        <label for="question-4-answers-C">C) Hepatitis</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D" />
+                        <label for="question-4-answers-D">D) Diabetes</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Webdevtrick Is For</h3>
+                    
+                    <div>
+                        <input type="radio" checked=":" name="question-5-answers" id="question-5-answers-A" value="A" />
+                        <label for="question-5-answers-A">A) Web Designer</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                        <label for="question-5-answers-B">B) Web Developer</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                        <label for="question-5-answers-C">C) Graphic Designer</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+                        <label for="question-5-answers-D">D) All Above These</label>
+                    </div>
+                
+                </li>
+            
+            </ol>
+            
+            <input type="submit" value="Submit" class="submitbtn" />
+ 
+ </form>
+ 
+ </div>
+ 
+ 
+</body>
+ 
+</html>
---
+++ games/resultado.php
@@ -0,0 +1,78 @@
+<!--code by webdevtrick (webdevtrick.com) -->
+<html>
+ 
+<head>
+ <meta charset=UTF-8" />
+ 
+ <title>Quiz - Resultado</title>
+ 
+ <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+ 
+<body>
+ 
+ <div id="page-wrap">
+ 
+ <h1>Resultado</h1>
+ 
+        <?php
+            
+            $answer1 = $_POST['question-1-answers'];
+            $answer2 = $_POST['question-2-answers'];
+            $answer3 = $_POST['question-3-answers'];
+            $answer4 = $_POST['question-4-answers'];
+            $answer5 = $_POST['question-5-answers'];
+        
+            $totalCorrect = 0;
+            
+            if ($answer1 == "C") { $totalCorrect++; 
+                echo " <div style ='font:20px Arial,tahoma,sans-serif;color:black'> 1. ¿En dónde suelen terminar los desechos electrónicos?";
+                echo "<br>";
+                echo"<div style ='font:20px Arial,tahoma,sans-serif;color:green'> Elegiste la respuesta correcta: Ríos";
+            }
+            if ($answer1 != "C") {
+                echo " <div style ='font:20px Arial,tahoma,sans-serif;color:black'> 1. ¿En dónde suelen terminar los desechos electrónicos?";
+                echo "<br>";
+                echo"<div style ='font:20px Arial,tahoma,sans-serif;color:red'> Elegiste una respuesta incorrecta, la respuesta correcta es: Ríos";
+            } 
+            if ($answer2 == "B") { $totalCorrect++; 
+                echo " <div style ='font:20px Arial,tahoma,sans-serif;color:black'> 2. ¿Cuál es una de las consecuencias de los desechos electrónicos?";
+                echo "<br>";
+                echo"<div style ='font:20px Arial,tahoma,sans-serif;color:green'> Elegiste la respuesta correcta: Contaminación en tierra, agua y aire";
+            }
+            if ($answer2 != "B") {
+                echo " <div style ='font:20px Arial,tahoma,sans-serif; color:black'> 2. ¿Cuál es una de las consecuencias de los desechos electrónicos?";
+                echo "<br>";
+                echo"<div style ='font:20px Arial,tahoma,sans-serif;color:red'> Elegiste una respuesta incorrecta, la respuesta correcta es: Contaminación en tierra, agua y aire";
+            } 
+            if ($answer3 == "D") { $totalCorrect++; 
+                echo " <div style ='font:20px Arial,tahoma,sans-serif;color:black'> 3. ¿Cuál se considera como uno de los materiales más contaminantes provenientes de desechos electrónicos?";
+                echo "<br>";
+                echo"<div style ='font:20px Arial,tahoma,sans-serif;color:green'> Elegiste la respuesta correcta: Mercurio";
+            }
+            if ($answer3 != "D") {
+                echo " <div style ='font:20px Arial,tahoma,sans-serif; color:black'> 3. ¿Cuál se considera como uno de los materiales más contaminantes provenientes de desechos electrónicos?";
+                echo "<br>";
+                echo"<div style ='font:20px Arial,tahoma,sans-serif;color:red'> Elegiste una respuesta incorrecta, la respuesta correcta es: Mercurio";
+            } 
+            if ($answer4 == "B") { $totalCorrect++; 
+                echo " <div style ='font:20px Arial,tahoma,sans-serif;color:black'> 4. ¿Cuál enfermedad se suele asociar con los desechos electrónicos?";
+                echo "<br>";
+                echo"<div style ='font:20px Arial,tahoma,sans-serif;color:green'> Elegiste la respuesta correcta: Asma";
+            }
+            if ($answer4 != "B") {
+                echo " <div style ='font:20px Arial,tahoma,sans-serif; color:black'> 4. ¿Cuál enfermedad se suele asociar con los desechos electrónicos?";
+                echo "<br>";
+                echo"<div style ='font:20px Arial,tahoma,sans-serif;color:red'> Elegiste una respuesta incorrecta, la respuesta correcta es: Asma";
+            } 
+            if ($answer5 == "D") { $totalCorrect++; }
+            
+            echo "<div style ='font:20px Arial,tahoma,sans-serif; color:black' id='results'>$totalCorrect / 5 correctas</div>";
+            
+        ?>
+ 
+ </div>
+ 
+</body>
+ 
+</html>
